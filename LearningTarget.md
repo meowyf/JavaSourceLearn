a8aa446bc5ee475b77788bc43ee0ec5d746c9e14
meowyf <meowyf@163.com>
2021-01-29 22:27:33 +0800

    first commit

---
+++ LearningTarget.md
@@ -0,0 +1,8 @@
+建议学习的源码：包结构
+source-io------(3) 普通IO 
+      -lang----(1) -annotation -reflect
+      -math----(5)
+      -net-----(7)
+      -nio-----(4) 非阻塞IO 需要基础
+      -time----(6)
+      -util----(2) -concurrent -function -stream
---
+++ src/com/meow/test/hashmap/TestHashMap001.java
@@ -0,0 +1,19 @@
+package com.meow.test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestHashMap001 {
+
+    public static void main(String[] args) {
+
+        Map<String, Double> test = new HashMap<>();
+
+        test.put("001", 0.02);
+        test.put("001", 0.02);
+        test.put("001", 0.02);
+        test.put("001", 0.02);
+        test.put("001", 0.02);
+
+    }
+}
